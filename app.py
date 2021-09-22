8771f1056d2d283aa04fb5942ffd01382221777f
yzhou43 <77255875+yzhou43@users.noreply.github.com>
2021-09-22 17:16:19 +0000

    add couse time

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     return render_template("index.html")
 
 app.run(
-    port=int(os.getenv("PORT", "8080")),
-    host=os.getenv("IP", "0.0.0.0")
+    port=int(os.getenv("PORT", "8080")), 
+    debug=True
 )
 
